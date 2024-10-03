a62b2cc634d147abe263d682779bc0182dfedd6f
Artem Lykhatskyi <tem4ik20.1@gmail.com>
2024-10-03 19:41:10 +0300

    complete lab 1

---
+++ src/main/java/com/example/back_end_java/model/Document.java
@@ -0,0 +1,102 @@
+package com.example.back_end_java.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "documents")
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DocumentType type;
+
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    @Column(nullable = false)
+    private LocalDate creationDate;
+
+    private LocalDate signedDate;
+
+    @Column(nullable = false)
+    private String userLogin;
+    // Constructors, getters, setters
+
+    public Document(Long id, String title, DocumentType type, String body, LocalDate creationDate, LocalDate signedDate, String userLogin) {
+        this.id = id;
+        this.title = title;
+        this.type = type;
+        this.body = body;
+        this.creationDate = creationDate;
+        this.signedDate = signedDate;
+        this.userLogin = userLogin;
+    }
+
+    public Document() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public DocumentType getType() {
+        return type;
+    }
+
+    public void setType(DocumentType type) {
+        this.type = type;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDate getSignedDate() {
+        return signedDate;
+    }
+
+    public void setSignedDate(LocalDate signedDate) {
+        this.signedDate = signedDate;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+}
